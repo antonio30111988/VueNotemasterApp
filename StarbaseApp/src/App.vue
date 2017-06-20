41fb1755e6d683d89aa4cd4506e2967b72f6197f
Antonio <antonio.conipo@gmail.com>
2017-06-20 13:47:06 +0200

    Added Profile component and added fetaure to display random profiles by click

--- StarbaseApp/src/App.vue
+++ StarbaseApp/src/App.vue
@@ -1,17 +1,31 @@
 <template>
 	<div id="app">
 		<h3>{{title}}</h3>
+		<div class="col-md-12"
+			<Profile 
+				v-for="(id, index) in ids"
+					:id="id" 
+					key="index"
+			/> 
+		</div>
 	</div>
 </template>
 
 <script>
+import axios from 'axios';
+import Profile from './components/Profile.vue';
+
 export default {
 	name:'app',
 	data(){
 		return {
 			title:"Generate Your Team",
-
+			ids: [1,7,15]
 		}
-	}
+	},
+	components:{
+		Profile
+	} 
 }
+
 </script>
---
+++ StarbaseApp/src/components/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="" @click="switchProfile">
+		{{profile}}
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+	props:['id'],
+	data(){
+		return {
+			profile:{}, 
+		}
+	},
+	methods:{
+		getProfiles(id){
+			axios.get('http://swapi.co/api/people/'+id)
+			.then(response => {
+				this.profile=response.data
+			})
+			.catch(e => {
+				console.log(e);
+			})
+		},
+		switchProfile(){
+			//random id from 0.33
+			let random_id=Math.floor(Math.random() * 83)+1 
+			this.getProfiles(random_id)
+		} 
+	},
+	created(){
+		this.getProfiles(this.id)
+	}
+}
+</script>
