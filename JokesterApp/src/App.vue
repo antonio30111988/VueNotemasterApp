a8ecc51f20d932ade6e452f0c0c4784ec5e400f6
Antonio <antonio.conipo@gmail.com>
2017-06-27 20:35:24 +0200

    Filter joke by three tpes by checkboxes on user click. Realized using simple v-show diectibe by two types array include check

--- JokesterApp/src/App.vue
+++ JokesterApp/src/App.vue
@@ -4,10 +4,20 @@
 		<button class="btn btn-primary" @click="initJokes">Add ten jokes</button>
 		<button class="btn btn-success" @click="addJoke">Add joke</button>
 		<br>
+		<span v-for="(type,index) in types">
+			<input 
+				type="checkbox"
+				v-model="checkTypes" 
+				:value="type" 
+				checked 
+			/>
+			<label>{{type}}</label>&nbsp;
+		</span>
 		<br>
 		<div class="col-md-12">
 			<Joke v-for="(joke,index) in $store.state.jokes"
 				key="index"
+				v-show="checkTypes.includes(joke.type)"
 				:index="index"
 				:joke="joke"
 			/> 
@@ -21,6 +31,12 @@ import {mapActions} from 'vuex'
 import Joke from './components/Joke.vue'
 
 	export default{
+		data(){
+			return {
+				types: ['general','knock-knock','programming'],
+				checkTypes: ['general','knock-knock','programming']
+			}
+		},
 		methods:mapActions([
 			'initJokes',
 			'addJoke',
